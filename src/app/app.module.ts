aef627be66d4198168f00a6ee8b3d389eac79f9c
Sana Aarouch <59958945+sanaarouch@users.noreply.github.com>
2025-09-25 15:53:45 +0200

    Modernize Angular project to resolve RouterModule compatibility issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from "@angular/router";
-import {HttpClientModule} from "@angular/common/http";
-import { FormsModule} from "@angular/forms";
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './commun/menu/menu.component';
@@ -15,9 +15,6 @@ import { ListeComponent } from './back/article/liste/liste.component';
 import { MenuDashboardComponent } from './back/menu-dashboard/menu-dashboard.component';
 import { AjouterComponent } from './back/article/ajouter/ajouter.component';
 
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,26 +26,19 @@ import { AjouterComponent } from './back/article/ajouter/ajouter.component';
     ListeComponent,
     MenuDashboardComponent,
     AjouterComponent,
-  
-
-  
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path : "" , component: AccueilComponent} , // page d'accueil
-      {path : "article/:id" , component: ArticleComponent} ,
-      {path : "admin/article/liste" , component : ListeComponent} ,
-      {path : "admin/article/ajouter" , component : AjouterComponent} ,
-      {path : "admin" , component : DashboardComponent} ,//admin apres
-      {path : "**" , component: NotFoundComponent} , // not found tjr à la fin
-     
-    ])
+      { path: '', component: AccueilComponent },
+      { path: 'article/:id', component: ArticleComponent },
+      { path: 'admin/article/liste', component: ListeComponent },
+      { path: 'admin/article/ajouter', component: AjouterComponent },
+      { path: 'admin', component: DashboardComponent },
+      { path: '**', component: NotFoundComponent }
+    ], { enableTracing: false })
   ],
-
-  providers: [ArticleFictifService],
+  providers: [],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
--- src/app/service/article-fictif.service.ts
+++ src/app/service/article-fictif.service.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleFictifService {
-  // URL de fallback si l'API Heroku ne fonctionne plus
-  url = "https://jsonplaceholder.typicode.com/posts";
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllArticles()
-  {
+  getAllArticles() {
     return this.http.get(this.url);
   }
 
-  getArticleById(id)
-  {
-    return this.http.get(this.url+"/"+id);
+  getArticleById(id: string | number) {
+    return this.http.get(`${this.url}/${id}`);
   }
 }
