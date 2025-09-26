5fb7707d637a677f085a060044fcb5b5c984ab48
Sana Aarouch <59958945+sanaarouch@users.noreply.github.com>
2025-09-26 15:18:56 +0200

    Updated article.component.ts

--- src/app/front/article/article.component.ts
+++ src/app/front/article/article.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import { ProductService, Product } from "../../service/article-fictif.service"
+import { ProductService, Product } from "../../service/article-fictif.service";
 
 @Component({
   selector: 'app-product',
@@ -34,6 +34,4 @@ export class ProductComponent implements OnInit {
   addToCart() {
     console.log(`Ajout au panier: ${this.quantity} x ${this.product?.title}`);
   }
-}
-
 }
